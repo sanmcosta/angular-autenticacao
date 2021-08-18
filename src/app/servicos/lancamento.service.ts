99114d700605465a358ae6c1c728fcae6cf67121
sanmcosta <sanmcosta@yahoo.com.br>
2021-08-18 14:09:29 -0300

    Adicionado exportar CSV

--- src/app/servicos/lancamento.service.ts
+++ src/app/servicos/lancamento.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { environment as env } from 'src/environments/environment';
 import {Lancamentos} from '../models/lancamento.model';
 import { HttpService } from './http.service';
+import { saveAs } from 'file-saver';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,22 @@ export class LancamentoService {
   listarTodos(): Observable<any> {
     const id = this.httpService.obterIdUsuario();
     return this.http.get(env.apiLancamentoUrl+ id,this.httpService.headers());
+  }
+
+  downloadCSV(lancamentos: Lancamentos[]) {
+   
+    
+    const colunas = 'ID,Data,Hora,Tipo,Localização\n';
+    const linhas: string[] = [];
+    lancamentos.forEach(lanc => {
+      const dataHora = lanc.data.split(' ');
+      const linha = `${lanc.id},${dataHora[0]},${dataHora[1]},${lanc.tipo},"${lanc.localizacao}"`;
+      linhas.push(linha);
+    });
+    //console.log(colunas + linhas.join('\n'));
+    const dados = colunas + linhas.join('\n');
+    const blob = new Blob([dados], { type: 'text/csv' });
+    saveAs(blob, 'lancamentos.csv');
   }
 
 }
